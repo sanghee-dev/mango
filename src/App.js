8866a3ceaff81846203368e07ba905b73717e440
leeedev <leeesangheee@gmail.com>
2020-11-21 14:11:20 +0900

    Add genres

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
@@ -37,8 +38,10 @@ class App extends React.Component {
               return (
                 <Movie
                   key={movie.id}
+                  id={movie.id}
                   year={movie.year}
                   title={movie.title}
+                  genres={movie.genres}
                   summary={movie.summary}
                   poster={movie.medium_cover_image}
                 />
--- src/Movie.js
+++ src/Movie.js
@@ -1,13 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Movie({ year, title, summary, poster }) {
+function Movie({ year, title, genres, summary, poster }) {
   return (
     <div className="movie">
       <img className="movie__image" src={poster} alt={title} title={title} />
       <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
+        <ul className="movie__genres">
+          {genres.map((genre, index) => (
+            <li className="movie__genres__genre" key={index}>
+              {genre}
+            </li>
+          ))}
+        </ul>
         <p className="movie__summary">{summary}</p>
       </div>
     </div>
@@ -18,6 +25,7 @@ Movie.propTypes = {
   id: PropTypes.number.isRequired,
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
 };
