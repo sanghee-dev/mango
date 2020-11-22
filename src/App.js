0c0817bdde99fe760875201ec1085817a0e09a56
leeedev <leeesangheee@gmail.com>
2020-11-22 21:29:22 +0900

    Reset <a> css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
 import Navigation from "./components/Navigation";
+import "./App.css";
 
 function App() {
   return (
